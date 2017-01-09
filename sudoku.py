a123c8c69418c82433d6a705723ac47c608d6381
thhollev <thhollev.hollevoet@ugent.be>
2017-01-09 13:01:17 +0100

    add sudoku

---
+++ sudoku.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python2.7
+
+import numpy as np
+import sys
+
+def findNextCellToFill(grid, i, j):
+        for x in range(i,9):
+                for y in range(j,9):
+                        if grid[x][y] == 0:
+                                return x,y
+        for x in range(0,9):
+                for y in range(0,9):
+                        if grid[x][y] == 0:
+                                return x,y
+        return -1,-1
+
+
+def isValid(grid, i, j, e):
+        rowOk = all([e != grid[i][x] for x in range(9)])
+        if rowOk:
+                columnOk = all([e != grid[x][j] for x in range(9)])
+                if columnOk:
+                        # finding the top left x,y co-ordinates of the section containing the i,j cell
+                        secTopX, secTopY = 3 *(i/3), 3 *(j/3)
+                        for x in range(secTopX, secTopX+3):
+                                for y in range(secTopY, secTopY+3):
+                                        if grid[x][y] == e:
+                                                return False
+                        return True
+        return False
+
+
+def solveSudoku(grid, i=0, j=0):
+        i,j = findNextCellToFill(grid, i, j)
+        if i == -1:
+                return True
+        for e in range(1,10):
+                if isValid(grid,i,j,e):
+                        grid[i][j] = e
+                        if solveSudoku(grid, i, j):
+                                return True
+                        # Undo the current cell for backtracking
+                        grid[i][j] = 0
+        return False
+
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        arr = [
+        [5,3,0,0,7,0,0,0,0],
+        [6,0,0,1,9,5,0,0,0],
+        [0,9,8,0,0,0,0,6,0],
+        [8,0,0,0,6,0,0,0,3],
+        [4,0,0,8,0,3,0,0,1],
+        [7,0,0,0,2,0,0,0,6],
+        [0,6,0,0,0,0,2,8,0],
+        [0,0,0,4,1,9,0,0,5],
+        [0,0,0,0,8,0,0,7,9]]
+        print("Need a 2D 9x9 array!\n")
+        print("e.g.")
+        print(np.matrix(arr))
+        print("\n SOLVED -> "+str(solveSudoku(arr))+"\n")
+        print(np.matrix(arr))
+    else:
+        solveSudoku(sys.argv[1])
+        print(np.matrix(sys.argv[1]))
